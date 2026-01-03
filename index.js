e438eafa766eb9672001455dc07471dd98e65167
rahulkumar083 <136946107+rahulkumar083@users.noreply.github.com>
2026-01-03 14:13:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//INCREAMENT OPRATOR
+var a=10;
+console.log(a);
+a++;
+console.log(a);
+a++;
+a++;
+console.log(a);
+
+var a1=10;
+++a1
+++a1
+console.log(a1);
+console.log(a1);
+++a1
+console.log(a1);
+
+var a2=20
+++a2
+a2++
+console.log(a2);
+console.log("DICRIMENT OPRATOR");
+//DICRIMENT OPRATOR
+var b=10
+console.log(b);
+b--;
+console.log(b);
+b--
+b--
+
+console.log(b);
+console.log("new way")
+var d=30
+d++
+++d
+d--
+--d
+console.log(d)
